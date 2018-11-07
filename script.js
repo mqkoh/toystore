032ea083b2013ee157db4350057f7591d53efbfc
mqkoh <30967444+mqkoh@users.noreply.github.com>
2018-11-07 21:45:26 +0800

    Add category buttons

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("banner");
+  var y = document.getElementsByClassName("navigate");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";
+     y[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block";
+  y[slideIndex-1].style.display = "block";
 }
