cd649f3c57805a7d3ec1a2e9a281fe38f21fc371
JYiiii <44420839+JYiiii@users.noreply.github.com>
2018-11-09 17:55:23 +0800

    Add password.php
    have not retrive customer info in the form.

---
+++ password.php
@@ -0,0 +1,144 @@
+<?php
+
+// This page lets customer change their password.
+
+$page_title = 'Update your details';
+
+// Check for form submission:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	require ('mysqli_connect.php'); // Connect to the db.
+		
+	$errors = array(); // Initialize an error array.
+	
+	// Check for a customer Name:
+	if (empty($_POST['custName'])) {
+		$errors[] = 'You forgot to enter your name.';
+	} else {
+		$cn = mysqli_real_escape_string($dbc, trim($_POST['custName'])); // capture the string
+	}
+	
+	//Check for customer gender
+	if (empty($_POST['custGender'])){
+		$errors[] = 'You forgot to pick your gender.';
+	}else{
+		$g = mysqli_real_escape_string($dbc, trim($_POST['custGender']));
+	}
+	
+	// Check for an email address:
+	if (empty($_POST['custEmail'])){
+		$errors[] = 'You forgot to enter your email address.';
+	}else{
+		$e = mysqli_real_escape_string($dbc,trim($_POST['custEmail']));
+	}
+	
+	// Check for phone number:
+	if (empty($_POST['custPhone'])){
+		$errors[] = 'You forgot to enter your phone number.';
+	}else{
+		$cp = mysqli_real_escape_string($dbc, trim($_POST['custPhone']));
+	}
+	
+	// Check for address:
+	if(empty($_POST['custAdd'])){
+		$errors[] = 'You forgot to enter your address.';
+	}else{
+		$a = mysqli_real_escape_string($dbc, trim($_POST['custAdd']));
+	}
+	
+	// Check for the current password:
+	if (empty($_POST['custPW'])){
+		$errors[] = 'You forgot to enter your password,';
+	}else{
+		$p = mysqli_real_escape_string($dbc,trim($_POST['custPW']));
+	}	
+
+	// Check for a new password and match 
+	// against the confirmed password:
+	if (!empty($_POST['pass1'])){
+		if($_POST['pass1'] != $_POST['pass2']){
+			$errors[] = 'Your new password did not match the confirmed password.';
+		}else {
+			$np = mysqli_real_escape_string($dbc,trim($_POST['pass1']));
+		}
+	}else {
+		$errors[] = 'You forgot to enter your new password.';
+	}
+	 	 		
+		// Check that they've entered the right email address/password combination:
+		$q = "SELECT custID,custName,custGender,custEmail,custPhone,custAdd FROM customer WHERE (custEmail='$e' AND custPW=SHA1('$p') )";
+		$r = @mysqli_query($dbc, $q);
+		$num = @mysqli_num_rows($r);
+		if ($num == 1) { // Match was made.
+	
+			// Get the user_id:
+			$row = mysqli_fetch_array($r, MYSQLI_NUM);
+
+			// Make the UPDATE query:
+			$q = "UPDATE customer SET custPW=SHA1('$np') WHERE custID=$row[0]";		
+			$r = @mysqli_query($dbc, $q);
+			
+			if (mysqli_affected_rows($dbc) == 1) { // If it ran OK.
+
+				// Print a message.
+				echo '<h1>Thank you!</h1>
+				<p>Your details have been updated.</p><p><br /></p>';	
+
+			} else { // If it did not run OK.
+
+				// Public message:
+				echo '<h1>System Error</h1>
+				<p class="error">Your password could not be changed due to a system error. We apologize for any inconvenience.</p>'; 
+	
+				// Debugging message:
+				echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
+	
+			}
+
+			mysqli_close($dbc); // Close the database connection.
+
+			// Include the footer and quit the script (to not show the form).
+			exit();
+				
+		} else { // Invalid email address/password combination.
+			echo '<h1>Error!</h1>
+			<p class="error">The email address and password do not match those on file.</p>';
+		}
+		
+	} else { // Report the errors.
+
+		echo '<h1>Error!</h1>
+		<p class="error">The following error(s) occurred:<br />';
+		foreach ($errors as $msg) { // Print each error.
+			echo " - $msg<br />\n";
+		}
+		echo '</p><p>Please try again.</p><p><br /></p>';
+	
+	} // End of if (empty($errors)) IF.
+
+	mysqli_close($dbc); // Close the database connection.
+		
+} // End of the main Submit conditional.
+?>
+<h1>Update your details</h1>
+<form action="password.php" method="post">
+	<fieldset>
+	<legend><b>Customer Details</b></legend>
+	<p>Name&nbsp;: <input type="text" name="custName" size="20" maxlength="60" value="'.$row[].'"  /></p>
+	<p>Gender&nbsp;: <input type="radio" name="custGender" <?php if (isset($_POST['custGender']) && ($_POST['custGender']) == "M") echo $_POST['custGender'];?> value="M" /> Male
+					 <input type="radio" name="custGender" <?php if (isset($_POST['custGender']) && ($_POST['custGender']) == "F") echo $_POST['custGender'];?> value="F"  />Female<br></p>
+	<p>Email Address&nbsp;: <input type="text" name="custEmail" size="20" maxlength="60" value="<?php if (isset($_POST['custEmail'])) echo $_POST['custEmail']; ?>"  /> </p>
+	<p>Phone Number&nbsp;: <input type="tel" name="custPhone" size="20" maxlength="40" value="<?php if (isset($_POST['custPhone'])) echo $_POST['custPhone']; ?>" /></p>
+	<p>Address&nbsp;: <input type="text" name="custAdd" size="30" maxlength="75" value="<?php if (isset($_POST['custAdd'])) echo $_POST['custAdd']; ?>" /></p><br>
+	</fieldset>
+	<br/>
+	
+	<fieldset>
+	<legend><b>Login Details</b></legend>
+	<p>Current Password: <input type="password" name="custPW" size="10" maxlength="20" value="<?php if (isset($_POST['custPW'])) echo $_POST['custPW']; ?>"  /></p>
+	<p>New Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>"  /></p>
+	<p>Confirm New Password: <input type="password" name="pass2" size="10" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>"  /></p><br/>
+	</fieldset>
+	
+	<p><input type="submit" name="submit" value="Update Details" /></p>
+</form>
