402b7ffe805fb11793fe01fda8a6e074b7c2c2b2
mqkoh <30967444+mqkoh@users.noreply.github.com>
2018-11-13 22:21:37 +0800

    Edit javascript error

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// find current filename
+var url = window.location.pathname;
+var filename = url.substring(url.lastIndexOf('/')+1);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+if (filename == "index.html") {
+	var slideIndex = 1;
+	showDivs(slideIndex);
+
+	function plusDivs(n) {
+	  showDivs(slideIndex += n);
+	}
+
+	function showDivs(n) {
+	  var i;
+	  var x = document.getElementsByClassName("banner");
+	  var y = document.getElementsByClassName("navigate");
+	  if (n > x.length) {slideIndex = 1}    
+	  if (n < 1) {slideIndex = x.length}
+	  for (i = 0; i < x.length; i++) {
+	     x[i].style.display = "none";
+	     y[i].style.display = "none";
+	  }
+	  x[slideIndex-1].style.display = "block";
+	  y[slideIndex-1].style.display = "block";
+	}
+} else if (filename == "pop.html"){
+	function openTab(evt, tabName) {
+	    var i, tabcontent, tablinks;
+	    tabcontent = document.getElementsByClassName("tabcontent");
+	    for (i = 0; i < tabcontent.length; i++) {
+	        tabcontent[i].style.display = "none";
+	    }
+	    tablinks = document.getElementsByClassName("tablinks");
+	    for (i = 0; i < tablinks.length; i++) {
+	        tablinks[i].className = tablinks[i].className.replace(" active", "");
+	    }
+	    document.getElementById(tabName).style.display = "block";
+	    evt.currentTarget.className += " active";
+	}
+
+	// Get the element with id="defaultOpen" and click on it
+	document.getElementById("defaultOpen").click();
+
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("banner");
-  var y = document.getElementsByClassName("navigate");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-     y[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-  y[slideIndex-1].style.display = "block";
-}
 
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
