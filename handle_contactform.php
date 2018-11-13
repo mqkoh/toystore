837a9bb13ce685be5afdd587fbabccf8e0f7a558
mqkoh <30967444+mqkoh@users.noreply.github.com>
2018-11-13 10:27:45 +0800

    Edit php style format

--- handle_contactform.php
+++ handle_contactform.php
@@ -4,8 +4,6 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $comment = $_POST['comment'];
 
-echo '<html>';
-echo '<head>';
 echo '<link href="https://fonts.googleapis.com/css?family=Cuprum|Exo|Indie+Flower|Nunito" rel="stylesheet">';
 echo '<style>';
 echo "body {
@@ -16,12 +14,10 @@ echo "body {
 	margin: 20pt;
 	";
 echo '</style>';
-echo '</head>';
 echo '<h1>Thank you for your feedback!</h1><h2>We will send you a reply as soon as possible.</h2>';
 echo 'Name: ' . $name;
 echo '<br>Email: ' . $email;
 echo '<br>Comment: ' . $comment;
 echo '<br><br><a href="index.html">Return to home page.</a>';
-echo '</html>';
 		
 ?>
