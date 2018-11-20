5338a687cb4d120eac4f0b0447acab6285f4d3b3
angelene1010 <43595075+angelene1010@users.noreply.github.com>
2018-11-20 18:08:02 +0800

    edit sytle

--- adminLogin_page.inc.php
+++ adminLogin_page.inc.php
@@ -11,10 +11,35 @@ if (isset($errors) && !empty($errors)) {
 }
 
 // Display the form:
-?><h1>Login</h1>
+?>
+<h1>Admin Login</h1>
+<div>
 <form action="adminLogin.php" method="post">
 	<p>Email Address: <input type="text" name="adminEmail" size="20" maxlength="40" /> </p>
 	<p>Password: <input type="password" name="adminPW" size="20" maxlength="40" /></p>
 	<p><input type="submit" name="submit" value="Login" /></p>
 </form>
+
+</div>
 <style>
+h1{
+	font-family:Charcoal,Sans-serif;
+  	left: 0;
+  	top: 50%;
+  	width: 100%;
+  	text-align: center;
+ 	color: #000000;
+}
+
+div{
+	border:1px solid;
+	padding:10px;
+	box-shadow:5px 10px #888888;
+}
+body{
+	text-align:center;
+	background-color:#bf99d9;
+	padding-top:300px;
+	
+ }
+</style>
