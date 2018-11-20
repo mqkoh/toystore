a2c5551feda95df003a9b8ff87cb408a1fa35c7f
angelene1010 <43595075+angelene1010@users.noreply.github.com>
2018-11-20 23:01:42 +0800

    login cust

--- custLogin.php
+++ custLogin.php
@@ -36,3 +36,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 // Create the page:
 include ('custLogin_page.inc.php');
 ?>
+
+<style>
+h1{
+	font-family:Charcoal,Sans-serif;
+  	left: 0;
+  	top: 50%;
+  	width: 100%;
+  	text-align: center;
+ 	color: #000000;
+}
+
+p{
+	border:1px solid;
+	padding:10px;
+	box-shadow:5px 10px #888888;
+	background-color:#bf99d9;
+}
+body{
+	text-align:center;
+	padding-top:200px;
+	
+ }
+</style>
